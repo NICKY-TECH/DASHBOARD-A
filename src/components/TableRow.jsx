7ae681ba168c0fc2c5119bd19ddf67c7b5bc8d78
DevTinaTi <tinadesk8@gmail.com>
2024-01-27 19:05:17 +0100

    fixed

--- src/components/TableRow.jsx
+++ src/components/TableRow.jsx
@@ -32,7 +32,8 @@ function TableRow({ date, amount, status, name, img }) {
         className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white gap-2 text-r"
       >
         <td className=" mr-5">
-        <img className="rounded-full w-[2rem] h-[3rem]" src={img} alt="image description"/>
+        <img className=""
+    alt="" src={img} alt="image description"/>
         </td>
         <td>{name}</td>
       </th>
